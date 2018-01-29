7dcc97a89918abf69249c00dabfcb0034d703cdd
JustimusPrime <hirejustin@yahoo.com>
2018-01-29 14:37:24 -0700

    working with different variable types

---
+++ 006_variables.js
@@ -0,0 +1,16 @@
+// var fullName : string = "Jordan Hudgens";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Justin Murray";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Jordan", "Hudgens");
+// not sure what diff between var and let is, think scope is bigger in left
+// constant - cant be changed, unlike ruby. you're telling program with a const that you don't want to let it be changed after you set it
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,27 @@
+// var fullName : string = "Jordan Hudgens";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Justin Murray";
+// paidAccount = false;
+
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+
+function printName(f, l) {
+  let greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName("Jordan", "Hudgens");
+
+
+// var needs consistent data type, but can change data. Let won't even let you do that.
+// let can redefine but not redeclare
+// 
+// constant - cant be changed, unlike ruby. you're telling program with a const that you don't want to let it be changed after you set it
+
+
